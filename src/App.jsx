d6ac2a6cdf472d35026790879c26d84807b53052
Lucas Sousa Rosa <77813484+lucassr-dev@users.noreply.github.com>
2023-01-21 00:33:31 +0000

    alteração minhas informações

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Lucas Sousa Rosa",
+  title: "Web Develloper",
+  email: "lucassr.job@gmail.com",
+  gitHub: "https://github.com/lucassr-dev",
+  instagram: "https://instagram.com/lucassrjob",
+  linkedIn: "https://www.linkedin.com/in/lucassrjob/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@",
+  youTube: "@",
 };
 
 const primaryColor = "#4E567E";
